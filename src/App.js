43c7639e58506ae0c6d798ab3527c5f90f64a98c
unknown <paolomartinligsay@gmail.com>
2021-11-29 00:09:30 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Login from './components/Login'
+import BucketList from './components/BucketList'
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem('thebucketlistloggedin') === 'true' || false)
+  const [loggedInUser, setLoggedInUser] = useState({})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        loggedIn ? <BucketList setLoggedIn={setLoggedIn} loggedInUser={loggedInUser}/> : <Login setLoggedInUser={setLoggedInUser} setLoggedIn={setLoggedIn}/>
+      }
     </div>
   );
 }
